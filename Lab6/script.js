4f40de8e87e1cd104d7a0d930882ed71da12b7f6
gerardrosell <gerard.rosell.cardus@gmail.com>
2016-10-25 18:51:57 +0200

    campo + timer

--- Lab6/script.js
+++ Lab6/script.js
@@ -1,16 +1,11 @@
 $( document).ready(function(){
 
 	var contador=60;
-	var canvas=document.getElementById("mycanvas");
+	var canvas=document.getElementById("canvas1");
 	var ctx=canvas.getContext("2d");
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 
-	function digit(pos,num){
-		/*var canvas=document.getElementById("mycanvas");
-		var ctx=canvas.getContext("2d");*/
-		//genera dígit
-		
-		
+	function digit(pos,num){		
 		if(num==0){
 			//1
 			ctx.fillRect(pos+50, 25, 5, 30);
@@ -151,26 +146,22 @@ $( document).ready(function(){
 		}
 
 
-	function render(){
-		var canvas=document.getElementById("mycanvas");
-		var ctx=canvas.getContext("2d");
-		
-		var pos1=150;
+	function renderTimer(){
+		var pos1=0;
 		var pos2=pos1+50;
 		var pos3=pos2+55;
 		var pos4=pos3+50;
 		var pospunts=pos3+5;
 		
-		var min=parseInt(contador/60);//nomes l'enter
-		//min=min.toFixed(0);
+		//separa els 4 dígits
+		var min=parseInt(contador/60);
 		var dm=parseInt(min/10);
-		//dm=dm.toFixed(0);
 		var um=min%10;
 		var seg=contador%60;
 		var ds=parseInt(seg/10);
-		//ds=ds.toFixed(0);
 		var us=seg%10;
 
+		//buida els dígits
 		ctx.fillStyle="#FFFFFF";
 		cleardigit(pos1);
 		cleardigit(pos2);
@@ -178,12 +169,12 @@ $( document).ready(function(){
 		cleardigit(pos4);
 
 		//punts
-		//ctx.fillStyle="#567890";
 		ctx.fillStyle="#000000";
 		ctx.fillRect(pospunts, 45, 5, 5);
 		ctx.fillRect(pospunts, 65, 5, 5);
 		ctx.stroke();
 
+		//pinta els nous dígits
 		digit(pos1,dm);
 		digit(pos2,um);
 		digit(pos3,ds);
@@ -194,15 +185,31 @@ $( document).ready(function(){
 			contador=60;
 		}
 		contador=contador-1;
-
-		/*ctx.clearRect(0,0,canvas.width,canvas.height);
-		ctx.fillStyle="#336699";
-		ctx.fillRect(10+pos,10+pos,100,100);
-		pos=(pos+5) %100;
-		console.log(pos)*/
 	};
 
-	setInterval(render,1000);
+	setInterval(renderTimer,1000);
+	
+});
+
+$( document).ready(function(){
+
+	var canvas=document.getElementById("canvas2");
+	var ctx=canvas.getContext("2d");
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	
+	var img = document.getElementById("imatge")
+    var pat = ctx.createPattern(img, "repeat");
+    ctx.rect(0, 0, canvas.width,canvas.height);
+    ctx.fillStyle = pat;
+    ctx.fill();
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(399,0,2,400);
+	
+	function render(){
+				
+	};
+
+	//setInterval(render,1000);
 	
 });
 
