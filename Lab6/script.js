a3b7c42e6ebfc65e3c3efcbf5c295f5644defe6a
gerardrosell <gerard.rosell.cardus@gmail.com>
2016-10-26 16:15:07 +0200

    anterior + pales en moviment + bola 1a versió

--- Lab6/script.js
+++ Lab6/script.js
@@ -213,44 +213,99 @@ $( document).ready(function(){
 	
 });*/
 
-$( document).ready(function(){
-	var Pala = function(x_start){
-		this.color_pala = "#000000";
-		this.position = {x:x_start, y:0};
-		this.size = {w:20, h:100};
-	};
+$( document ).ready(function() {
+    var Pala = function(x_start,y_end){
+        this.color_pala = "#000000";
+        this.position = {x:x_start, y:0};
+        this.size = {w:10, h:100};
+        this.y_end = y_end;
+    };
+    Pala.prototype.render = function(ctx2){
+        ctx2.fillStyle = this.color_pala;
+        ctx2.fillRect(   this.position.x,
+                        this.position.y,
+                        this.size.w,
+                        this.size.h);
+    };
+    Pala.prototype.goUp = function(){
+        if(this.position.y >= 0) this.position.y -= 10;
+    };
+    Pala.prototype.goDown = function(){
+        if(this.position.y+this.size.h <= this.y_end) this.position.y += 10;
+    };
+    Pala.prototype.setKeys = function(keyUp, keyDown){
+        var _this = this;
+        $(window).keydown(function(event) {
+            //console.log("Key pressed is: " +event.which);
+            if ( event.which == keyUp ) {
+                _this.goUp()
+            }else if( event.which == keyDown ){
+                _this.goDown();
+            }
+        });
+    }
 
-	Pala.prototype.render = function(ctx){
-		ctx.fillStyle = this.color_pala;
-		ctx.fillRect(	this.position.x,
-						this.position.y,
-						this.size.w,
-						this.size.h)
-	};
-	var canvas = document.getElementById("canvas2");
-	var ctx = canvas.getContext("2d");
-	var pala_L = new Pala(10);
-	var pala_R = new Pala(canvas.width-30);
+    var Bola = function(start_pos_x, start_pos_y){
+        this.position = {x:start_pos_x, y:start_pos_y};
+        this.color_bola = "#FF0000";
+        this.size = {w:12, h:12};
+        this.angle =  170;
+    }
+    Bola.prototype.render = function(ctx2){
+        ctx2.fillStyle = this.color_bola;
+        ctx2.fillRect(   this.position.x,
+                        this.position.y,
+                        this.size.w,
+                        this.size.h);
+    }
+
+	var canvas2 = document.getElementById("canvas2");
+	var ctx2 = canvas2.getContext("2d");
+	var pala_L = new Pala(10,canvas2.height);
+	var pala_R = new Pala(canvas2.width-30,canvas2.height);
+
+	pala_L.setKeys(81,65); // Keys: Q, A
+    pala_R.setKeys(87,83); // Keys: W, S
 	
+	var bola = new Bola(canvas2.width/2, canvas2.height/2);
+
+	function updateBola(){
+
+        if(bola.position.y < 10){
+            bola.angle -=180;
+        } else if(bola.position.y > canvas2.height){
+            bola.angle -=180;
+        }
+
+        bola.position.x += Math.sin(bola.angle * Math.PI / 180.0) * 3;
+        bola.position.y += Math.cos(bola.angle * Math.PI / 180.0) * 3;
+    }
 
 	function renderCamp(){
+
+		ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+		
 	//pinta campo
 		var img = document.getElementById("imatge")
-	    var pat = ctx.createPattern(img, "repeat");
-	    ctx.rect(0, 0, canvas.width,canvas.height);
-	    ctx.fillStyle = pat;
-	    ctx.fill();
-	    ctx.fillStyle = "#000000";
-	    ctx.fillRect(399,0,2,400);	
-	    ctx.beginPath();
-		ctx.arc(400, 200, 80, 0, 2 * Math.PI);
-		ctx.arc(400, 200, 81, 0, 2 * Math.PI);
-		ctx.stroke();	
+	    var pat = ctx2.createPattern(img, "repeat");
+	    ctx2.rect(0, 0, canvas2.width,canvas2.height);
+	    ctx2.fillStyle = pat;
+	    ctx2.fill();
+	    ctx2.fillStyle = "#000000";
+	    ctx2.fillRect(399,0,2,400);	
+	    ctx2.beginPath();
+		ctx2.arc(400, 200, 80, 0, 2 * Math.PI);
+		ctx2.arc(400, 200, 81, 0, 2 * Math.PI);
+		ctx2.stroke();
+
 
 		//pinta pala
-		/*ctx.clearRect(0, 0, canvas.width, canvas.height);*/
-		pala_L.render(ctx);
-		pala_R.render(ctx);
+		updateBola();
+		//ctx2.rect(0, 0, canvas2.width,canvas2.height);
+		
+		pala_L.render(ctx2);
+		pala_R.render(ctx2);
+		bola.render(ctx2);
 	};
 
 	setInterval(renderCamp,100);
